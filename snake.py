fc5b46ad373016303be65d4fb11d395ac31757fb
Aryan singh <144314419+Tyso@users.noreply.github.com>
2024-10-28 10:21:51 +0530

    Create snake.py

---
+++ snake.py
@@ -0,0 +1,20 @@
+import random
+print("Type integer for required: ")
+print("0.snake")
+print("1.water")
+print("2.gun")
+b=int(input("enter number between 0,1,2 : "))
+match b:
+    case 0:
+        print("user input : snake")
+    case 1:
+        print("user input : water")   
+    case 2:
+        print("user input : gun")     
+a=random.randint(0,2)
+if a==0:
+    print("computer : snake")
+elif a==1:
+    print("computer : water")
+else:
+    print("computer : gun")
